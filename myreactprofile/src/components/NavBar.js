25c3eacf75b66318c341463b5c07da6f002c5299
Andrew Garcia <andrewagarcia8@gmail.com>
2022-05-09 18:44:07 -0700

    adding components files

---
+++ myreactprofile/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const NavBar = () => {
+
+return (
+    <div className="NavBar">
+        <h1>Andrew Garcia</h1>
+        <div className="nav-links">
+            <a href=".header">Home</a>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </div>    
+    );
+}
+
+export default NavBar; 
