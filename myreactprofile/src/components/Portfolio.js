358b804a5a2c69caf044598b26cb16eb004a82bb
Andrew Garcia <andrewagarcia8@gmail.com>
2022-05-12 18:42:57 -0700

    Creating projects

--- myreactprofile/src/components/Portfolio.js
+++ myreactprofile/src/components/Portfolio.js
@@ -1,36 +1,35 @@
 import React, { useState } from 'react';
-import Projects from '../components/Projects';
+import Project from '../components/Projects';
 
 function Portfolio() {
 
-  // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'Food-For-Thought',
+      name: "food-for-thought",
       description: 'JavaScript and API',
       link: "https://jasonjayoo.github.io/Food-For-Thought",
       repo: "https://github.com/jasonjayoo/Food-For-Thought"
     },
     {
-      name: 'Group-Project-2',
+      name: 'group-project-2',
       description: 'MVC models and Backend',
       link: "https://pixel-warrior-app-grp-prj-2.herokuapp.com",
       repo: "https://github.com/aagarc9/Group-Project-2"
     },
     {
-      name: 'Los-Angeles-DT',
-      description: 'HTML/CSS',
+      name: 'weather-dashboard',
+      description: 'HTML/CSS/JAVASCRIPT',
       link: "https://github.com",
       repo: "https://github.com"
     },
     {
-      name: 'led-wall',
+      name: 'password-generator',
       description: 'Node/IoT',
       link: "https://github.com",
       repo: "https://github.com"
     },
     {
-      name: 'calculator',
+      name: 'social-api',
       description: 'React/JavaScript/CSS',
       link: "https://github.com",
       repo: "https://github.com"
@@ -40,10 +39,10 @@ function Portfolio() {
   return (
     <div>
       <div className="flex-row">
-        {projects.map((project, ) => (
-          <Projects
+        {projects.map((project, idx ) => (
+          <Project
             project={project}
-            key={"project"}
+            key={"project" + idx}
           />
         ))}
       </div>
--- myreactprofile/src/components/Projects.js
+++ myreactprofile/src/components/Projects.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { removeHyphensAndCapitalize } from '../utils/helpers';
 
-function Projects({ project }) {
+function Project({ project }) {
 
   const { name, repo, link, description } = project;
 
   return (
-    <div className="project" key={name}>
-      <img
-        src={`../assets/projects/${name}.jpg`}
-        alt={removeHyphensAndCapitalize(name)}
-        className="project-bg"
-      />
-      <div className="project-text">
+    <div class="project" key={name}>
+      <img src={require(`../assets/projects/${name}.jpg`)} alt={removeHyphensAndCapitalize(name)} class="project-img"/>
+      <div class="project-text">
         <h3>
           <a href={link}>{removeHyphensAndCapitalize(name)}</a>{' '}
           <a href={repo}>
@@ -25,4 +21,4 @@ function Projects({ project }) {
   );
 }
 
-export default Projects;
+export default Project;
